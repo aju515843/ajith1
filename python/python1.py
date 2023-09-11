e07ed18da8272ef0a10110874bc3e05fabb4078f
Ajith AR <83577676+aju515843@users.noreply.github.com>
2023-09-11 08:58:10 +0530

    work

---
+++ python/python1.py
@@ -0,0 +1,81 @@
+'''''''''x=10
+y=20
+print(float(x))
+print(float(y))
+print(complex(y))
+print(complex(x))
+print(int(x))
+'''''
+'''
+a=5
+b=6'''
+
+'''a+=5
+b-=5
+print(a)
+print(b)'''
+
+'''a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+'''
+'''a,b=b,a
+print(a,b)'''
+
+'''if(10<11):
+  print("greater")'''
+
+
+
+
+#if(6>5):
+#       print("greater")
+#else:
+#       print("smaller")
+ 
+ #find greater number the 3 digits given by the user?
+# a=int(input("enter the first value"))
+# b=int(input("enter the second value"))
+# c=int(input("enter the 3rd value"))    
+# if(a>b):
+#     if(a>c):
+#         print("first value is greater")
+#     else:
+#          print("3rd value is greater")
+# elif(b>c):
+#     print("second value is greater")
+# else:
+#     print("3rd value is greater") 
+#check whether the given number is even or odd?
+# a=int(input("enter the value"))
+# if(a%2==0):
+#    print("number is odd")
+# else:
+#    print("number is even")
+#leap year?
+# a=int(input("enter year"))
+# if(a%4==0):
+#     print("is a leap year")
+# else:
+#     print("is not a leap year")
+
+#tuple
+# t=(1,2,3,4,5,"hai")
+# y=list(t)
+# y[5]="hello"
+# t=tuple(y)
+# print(t)   
+
+#list
+# list1=[1,2,9,4,5,6,7]
+# list1[3]=3
+# print(list1)
+
+#set
+# s={1,2,3,4,5,"hai"}
+# y=list(s)
+# y[3]="hello"
+# s=set(y)
+# print(s)
