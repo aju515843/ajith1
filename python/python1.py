eae0b869d9d23f5f4992297a9228ad470f2a9ef4
Ajith AR <83577676+aju515843@users.noreply.github.com>
2023-09-12 08:53:31 +0530

    work

--- python/python1.py
+++ python/python1.py
@@ -1,31 +1,31 @@
-'''''''''x=10
-y=20
-print(float(x))
-print(float(y))
-print(complex(y))
-print(complex(x))
-print(int(x))
-'''''
-'''
-a=5
-b=6'''
+# x=10
+# y=20
+# print(float(x))
+# print(float(y))
+# print(complex(y))
+# print(complex(x))
+# print(int(x))
 
-'''a+=5
-b-=5
-print(a)
-print(b)'''
 
-'''a=a+b
-b=a-b
-a=a-b
-print(a)
-print(b)
-'''
-'''a,b=b,a
-print(a,b)'''
+# a=5
+# b=6
 
-'''if(10<11):
-  print("greater")'''
+# a+=5
+# b-=5
+# print(a)
+# print(b)
+
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a)
+# print(b)
+
+# a,b=b,a
+# print(a,b)
+
+# if(10<11):
+#   print("greater")
 
 
 
@@ -34,8 +34,10 @@ print(a,b)'''
 #       print("greater")
 #else:
 #       print("smaller")
+
+
  
- #find greater number the 3 digits given by the user?
+# ?)find greater number the 3 digits given by the user?
 # a=int(input("enter the first value"))
 # b=int(input("enter the second value"))
 # c=int(input("enter the 3rd value"))    
@@ -48,34 +50,80 @@ print(a,b)'''
 #     print("second value is greater")
 # else:
 #     print("3rd value is greater") 
-#check whether the given number is even or odd?
+
+
+
+# ?)check whether the given number is even or odd?
 # a=int(input("enter the value"))
 # if(a%2==0):
 #    print("number is odd")
 # else:
 #    print("number is even")
-#leap year?
+
+
+
+# ?)leap year?
 # a=int(input("enter year"))
 # if(a%4==0):
 #     print("is a leap year")
 # else:
 #     print("is not a leap year")
 
-#tuple
+
+
+# ?)tuple
 # t=(1,2,3,4,5,"hai")
 # y=list(t)
 # y[5]="hello"
 # t=tuple(y)
 # print(t)   
 
-#list
+
+
+# ?)list
 # list1=[1,2,9,4,5,6,7]
 # list1[3]=3
 # print(list1)
 
-#set
+
+
+# ?)set
 # s={1,2,3,4,5,"hai"}
 # y=list(s)
 # y[3]="hello"
 # s=set(y)
 # print(s)
+
+
+# ?) WAP to check whether the last digit of a number is divisible by 3 or not?
+# a=int(input("enter a number"))
+# if(a%3!=1):
+#   print("number is divisible by 3")
+# else:
+#   print("numbe not divisible by 3") 
+
+
+   
+# ?)display "HELLO" if entered number is divisible by 5 otherwise print bye
+# a=int(input("enter number"))
+# if(a%5!=1):
+#   print("HELLO")
+# else:
+#   print("Bye") 
+
+
+
+# ?)to accept percentage from the user and display the grade according following criteria
+# n=int(input("enter mark"))
+# if(n>90):
+#   print("A grade")
+# elif(n>80 and n<=90):
+#   print("B grade")
+# elif(n>=60 and n<=80):
+#   print("C grade")
+# elif(n<60):
+#   print("D grade")  
+
+
+
+   
